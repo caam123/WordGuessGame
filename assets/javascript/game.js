0ba1d412b02ae19e059225e1ad98a63498a3cb5c
Charlie <caam_1275@hotmail.com>
2019-03-15 22:50:03 -0600

    Reordenando para entender, guardo antes de cagarla

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,37 +3,49 @@
 
 //Lista de las razas
 var dogsArray = ["bulldog","corgi","dalmata","chihuahua","salchicha","boxer","labrador"];
+
+
 //Numero random solo porque luego lo escribo mal
 var randomNumber = Math.random();
 //Esta nos devuelve el index de uno de las razas dentro del array previamente creado
 var dogsIndex = Math.floor(randomNumber*dogsArray.length);
 //Este nos devuelve el "index" random que es de una raza dentro del array dogs
 var dogsRaza = dogsArray[dogsIndex];
-//Aqui se guarda el valor inicial de oportunidades de adivinar, disminuye 1 sino le atinas
-var guessesLeft = 9;
-// Se crea un array del largo de la raza a buscar, pero se sustiye cada index [i] con "_" y con join (despues) imprimimos sin comas. 
+
 var underscore = []
+// Se crea un array del largo de la raza a buscar, pero se sustiye cada index [i] con "_" y con join (despues) imprimimos sin comas. 
 for(var i=0; i<dogsRaza.length; i++){
     underscore[i] = "_";
 }
+var lettersGuessed = [];
+
+
+//Marcadores 
+var guessesLeft = 9;
+var wins= 0;
+var lost=0;
+
+
+
 
 //Esta linea imprime las rayitas
 document.getElementById("underscore").textContent = underscore.join("");
 //Esta linea imprime los guesses que te quedan
 document.getElementById("guesses").textContent = guessesLeft;
-
 //Pa saber que palabra magica es
 console.log(dogsRaza)
 
 //===== F U N C I O N ==============
 
-document.onkeyup = function StartGame (event){
+
+function startGame (event){
     // Es igual a la tecla que se oprime
     var letter=event.key;
     // Lo que hace el index es verificar si la letter (tecla presionada) existe dentro de la palabra random dogsRaza
     var index=dogsRaza.indexOf(letter);
     console.log(dogsRaza);
-     if(index!=-1){
+
+        if(index!=-1){
         for(var i=0; i<dogsRaza.length; i++){
             if (dogsRaza[i]===letter){
                 // console.log("se encontro letra",underscore)
@@ -46,26 +58,64 @@ document.onkeyup = function StartGame (event){
     }else{
         //Si no encuentra match entonces disminuye los guesses en -1
         guessesLeft--;
-        //Imprime los guesses left
+            //Imprime los guesses left
         document.getElementById("guesses").textContent=guessesLeft;
         //alerta que no es la letra
         alert("Nope! Try again");
-
     }
-/*     var lettersGuessed=[];
-        lettersGuessed.push(event.key);
-        document.getElementById("lettersGuessed").textContent=lettersGuessed;        
-        console.log(lettersGuessed); */
-};
 
-//====== OPCION TROPICALIZADA FUERA DE LA FUNCION GENERAL ¿? NO SE PORQUE =======/
-
-var lettersGuessed = [];
-    document.onkeypress = function (event){
     lettersGuessed.push(event.key);
     document.getElementById("lettersGuessed").textContent = lettersGuessed;
     console.log("array de guessed", lettersGuessed);
 };
+    
+//==== START OVER=====
+
+function startOver(){
+    guessesLeft = 9;
+    lettersGuessed = [];
+};
+
+
+function foundLetter(){
+    if(underscore.toString()==dogsRaza.toString()){
+        wins++;
+        console.log("marcador")
+        startOver();
+    }
+}
+
+function foundLetter(){
+    if(underscore.toString()==dogsRaza.toString()){
+        wins++;
+        console.log("marcador")
+        startOver();
+    }
+}
+
+
+
+
+
+
+//====================CEMEMENTERIO DE PRUEBAS Y ERRORES=======================================
+
+
+/*     var lettersGuessed=[];
+        lettersGuessed.push(event.key);
+        document.getElementById("lettersGuessed").textContent=lettersGuessed;        
+        console.log(lettersGuessed); */
+
+
+
+
+/*     document.onkeypress = function (event){
+    lettersGuessed.push(event.key);
+    document.getElementById("lettersGuessed").textContent = lettersGuessed;
+    console.log("array de guessed", lettersGuessed);
+}; */
+
+
 
 
 
